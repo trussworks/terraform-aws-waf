a272bcb77daee3fe48e6acedb6ce0f90c3fd657b
pjdufour-truss <patrick.dufour@truss.works>
2019-11-04 14:31:08 -0500

    removed extra quotes

--- variables.tf
+++ variables.tf
@@ -6,23 +6,23 @@ variable "associate_alb" {
 
 variable "alb_arn" {
   description = "ARN of the Application Load Balancer (ALB) to be associated with the Web Application Firewall (WAF) Access Control List (ACL)."
-  type        = "string"
+  type        = string
 }
 
 variable "wafregional_rule_f5_id" {
   description = "The ID of the F5 Rule Group to use for the WAF for the ALB.  Find the id with \"aws waf-regional list-subscribed-rule-groups\"."
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "regex_path_disallow_pattern_strings" {
   description = "The list of URI paths to block using the WAF."
-  type        = "list"
+  type        = list
 }
 
 variable "regex_host_allow_pattern_strings" {
   description = "The list of hosts to allow using the WAF (as found in HTTP Header)."
-  type        = "list"
+  type        = list
 }
 
 variable "ip_rate_limit" {
@@ -33,16 +33,16 @@ variable "ip_rate_limit" {
 
 variable "ip_sets" {
   description = "List of sets of IP addresses to block."
-  type        = "list"
+  type        = list
   default     = []
 }
 
 variable "web_acl_name" {
   description = "Name of the Web ACL"
-  type        = "string"
+  type        = string
 }
 
 variable "web_acl_metric_name" {
   description = "Metric name of the Web ACL"
-  type        = "string"
+  type        = string
 }
