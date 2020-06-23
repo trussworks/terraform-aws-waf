98bbb94745f05838b9c357c091ed42c32af68b44
qrenaud <quentin.renaud@ontexglobal.com>
2020-06-23 16:30:43 +0200

    Update: From cloudformation to terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,299 @@
-resource "aws_cloudformation_stack" "waf" {
-  name = "waf-stack"
-  parameters = {
-    ProjectName = var.project,
-    EnvName     = var.env
-    WAFName     = "${var.project}-${var.env}-waf-web-acl"
-    ALBARN      = var.alb
+resource "aws_wafv2_regex_pattern_set" "regex" {
+  name        = "${var.project}-${var.env}-regex-pattern"
+  description = "Regex pattern"
+  scope       = "REGIONAL"
+
+  regular_expression {
+    regex_string = "mag|guide-de-la-couche|media|static|admin_t0bdll|apis|locker"
   }
 
-  template_body = file("${path.module}/cloudformation.yaml")
+  tags = var.tags
+}
+
+resource "aws_wafv2_ip_set" "ip" {
+  name               = "${var.project}-${var.env}-ip-set-whitelist"
+  description        = "Whitelist IP set"
+  scope              = "REGIONAL"
+  ip_address_version = "IPV4"
+  addresses          = ["185.170.45.38/32"]
+
+  tags = var.tags
+}
+
+
+resource "aws_wafv2_web_acl" "waf" {
+  name        = "${var.project}-${var.env}-waf-web-acl"
+  description = "Waf ACL"
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "${var.project}-${var.env}-rule-ip-whitelist"
+    priority = 0
+
+    override_action {
+      allow {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 100
+        aggregate_key_type = "IP"
+
+        scope_down_statement {
+          not_statement  {
+            regex_pattern_set_reference_statement {
+              arn = aws_wafv2_regex_pattern_set.regex.arn
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.project}-${var.env}-rule-ip-whitelist"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "${var.project}-${var.env}-rule-ip-whitelist"
+    priority = 0
+
+    override_action {
+      allow {}
+    }
+
+    statement {
+      ip_set_reference_statement {
+        arn = aws_wafv2_ip_set.ip.arn
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.project}-${var.env}-rule-ip-whitelist"
+      sampled_requests_enabled   = true
+    }
+  }
+
+
+  rule {
+    name     = "${var.project}-${var.env}-rule-timeOne-allow"
+    priority = 1
+
+    override_action {
+      allow {}
+    }
+
+    statement {
+      byte_match_statement  {
+        field_to_match {
+          field_to_match {}
+        }
+        positional_constraint = "CONTAINS"
+        search_string = "utm_source=TimeOne"
+        text_transformation {
+          priority = 0
+          type = "None"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.project}-${var.env}-timeOne-allow"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+
+        excluded_rule {
+          name = "SizeRestrictions_QUERYSTRING"
+        }
+        excluded_rule {
+          name = "SizeRestrictions_BODY"
+        }
+        excluded_rule {
+          name = "SizeRestrictions_URIPATH"
+        }
+        excluded_rule {
+          name = "GenericRFI_QUERYARGUMENTS"
+        }
+        excluded_rule {
+          name = "GenericRFI_BODY"
+        }
+        excluded_rule {
+          name = "CrossSiteScripting_BODY"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesAnonymousIpList"
+    priority = 3
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAnonymousIpList"
+        vendor_name = "AWS"
+
+        excluded_rule {
+          name = "HostingProviderIPList"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesAnonymousIpList"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesAmazonIpReputationList"
+    priority = 4
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesAmazonIpReputationList"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 5
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesKnownBadInputsRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesPHPRuleSet"
+    priority = 6
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesPHPRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesPHPRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesLinuxRuleSet"
+    priority = 7
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesLinuxRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesLinuxRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesWordPressRuleSet"
+    priority = 8
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesWordPressRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesWordPressRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+
+  tags = var.tags
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${var.project}-${var.env}-waf-web-acl"
+    sampled_requests_enabled   = true
+  }
 }
